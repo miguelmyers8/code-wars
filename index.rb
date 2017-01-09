a30c4ffc7b1c92157d70cc4314dc624441479984
miguel myers <myers.m007@gmail.com>
2017-01-09 11:27:56 -0500

    basic op

--- index.rb
+++ index.rb
@@ -1,6 +1,7 @@
 
 # that takes as a parameter an array of numbers
 # represented as strings and outputs an array of numbers.
+
 def to_number_array(string_array)
 string_array.map {|x| x.to_i }
 i = 0
@@ -10,6 +11,7 @@ end
 #######
 #Finish the solution so that it returns the sum of all
 #the multiples of 3 or 5 below the number passed in.
+
 def solution(number)
 n = 0..number
 a = []
@@ -27,7 +29,23 @@ end
 ##########
 #You need to write a function that reverses the words in a
 #given string. If this is not clear enough, here are some examples:
+
 def reverse(string)
 s = string.split.reverse.join(" ")
   #your code here
+end
+
+
+############
+# The function should take three arguments - operation(string/char),
+# value1(number), value2(number). The function
+# should return result of numbers
+# after applying the chosen operation.
+# basic_op('+', 4, 7)         # Output: 11
+# basic_op('-', 15, 18)       # Output: -3
+# basic_op('*', 5, 5)         # Output: 25
+# basic_op('/', 49, 7)        # Output: 7
+
+def basic_op(operator, value1, value2)
+value1.send operator, value2
 end
